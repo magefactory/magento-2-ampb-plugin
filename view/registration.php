e999f45dc97284d853f00b0c779f11db2e6e5a95
magefactory <alex@alexander-naumov.com>
2021-09-28 14:12:57 +0200

    add new banner positions

---
+++ view/registration.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * @author Magefactory
+ * @copyright Copyright (c) 2020 Magefactory (https://magefactory.alexander-naumov.com)
+ * @package Magefactory_PluginAmastyPromoBanners
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magefactory_PluginAmastyPromoBanners',
+    __DIR__
+);
